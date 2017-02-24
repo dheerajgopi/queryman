7c12c63fce7a413d8034a5ab4b6e8250b707e5cf
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2017-02-24 19:00:12 +0530

    mysql connection problem.

--- app/databases/mysqldb.js
+++ app/databases/mysqldb.js
@@ -3,55 +3,44 @@
 const mysql = require ('mysql');
 
 class MysqlConnector {
-    constructor (host, user, pswd, dbName) {
+    constructor (host, user, pswd, dbName, connLimit) {
         this.host = host;
         this.user = user;
         this.pswd = pswd;
         this.dbName = dbName;
-        this.connection = mysql.createConnection ({
+        this.pool = mysql.createPool ({
             host: host,
             user: user,
             password: pswd,
-            database: dbName
-        });
-    }
-
-    connect () {
-        this.connection.connect((error) => {
-            if (error) {
-                throw error;
-            }
-        });
-
-        this.connection.on('close', (err) => {
-            // reconnect to mysql if connection is closed unexpectedly
-            if (err) {
-                this.connection = mysql.createConnection ({
-                    host: host,
-                    user: user,
-                    password: pswd,
-                    database: dbName
-                });
-            }
+            database: dbName,
+            connectionLimit: connLimit
         });
     }
 
     query (queryString) {
-        let res = this.connection.query(queryString);
+        this.pool.getConnection((errConn, connection) => {
+            console.log(connection);
+            if (errConn) {
+                connection.release();
+                throw errConn;
+            }
 
-        res.on('error', (err) => {
-            throw err;
+            let res = connection.query(queryString);
+
+            res.on('error', (err) => {
+                throw err;
+            });
+
+            res.on('result', (row) => {
+                connection.pause();
+                console.log(row);
+                connection.resume();
+            });
         });
-
-        res.on('result', (row) => {
-            this.connection.pause();
-            console.log(row);
-            this.connection.resume();
-        })
     }
 
     endConnect () {
-        this.connection.end();
+        this.pool.end();
     }
 
     toString () {
@@ -63,8 +52,7 @@ class MysqlConnector {
     }
 }
 
-const connector = new MysqlConnector('hostname', 'uname', 'pswd', 'dbname');
-connector.connect();
+const connector = new MysqlConnector('host', 'uname', 'pswd', 'dbname');
 connector.print();
-connector.query('select * from dbtable')
+connector.query('select * from dbtable');
 connector.endConnect();
