59955017984c2abf7a4b6dc405c1f94bbc4be866
dheerajgopi <dheerajgopinath@gmail.com>
2017-02-26 23:14:12 +0530

    install passport, bcrypt, cookieparser, bodyparser, morgan

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-const express = require('express');
+const express = require('express'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      session = require('express-session'),
+      passport = require('passport'),
+      localStrategy = require('passport-local'),
+      googleStrategy = require('passport-google'),
+      facebookStrategy = require('passport-facebook');
+
 const app = express();
 const port = 3000;
 
-app.use((request, response, next) => {
-    console.log(request.headers);
+//=============PASSPORT==============
+
+//=============EXPRESS===============
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(session({secret: 'secret', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// session persisted message middleware
+app.use((req, res, next) => {
+    var err = req.session.error,
+        msg = req.session.notice,
+        success = req.session.success;
+
+    delete req.session.error;
+    delete req.session.success;
+    delete req.session.notice;
+
+    if (err) res.locals.error = err;
+    if (msg) res.locals.notice = msg;
+    if (success) res.locals.success = success;
+
     next();
 });
-
-app.use((request, response, next) => {
-    request.chance = Math.random();
-    next();
-});
-
-app.get('/', (request, response) => {
-    throw new Error('oops!');
-    response.json({
-        chance: request.chance
-    });
-});
-
-app.use((err, request, response, next) => {
-    console.log(err);
-    response.status(500).json({
-        status: 500
-    });
-});
-
 
 app.listen(port, (err) => {
     if (err) {
